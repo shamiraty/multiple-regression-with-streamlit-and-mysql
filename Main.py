38357082b8c2b682ee7ed3c1f366beb816c14596
shamiraty <129072179+shamiraty@users.noreply.github.com>
2023-10-30 16:30:21 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 from sklearn.model_selection import train_test_split
-import os
 #from query import *
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
